a09fc12f9ce920cfb99bbfa004a2d160d14f28b6
7926619 <7926619@daum.net>
2018-11-29 18:59:06 +0900

    어후

---
+++ P2P/WebContent/goal_check.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@page import="java.util.ArrayList"%>
+<!DOCTYPE html>
+
+<jsp:useBean id="score" class="myinfo.ScoreBean" scope="session" />
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Goal Check</title>
+</head>
+<body>
+	<%
+		System.out.println(request.getParameter("input_goal"));
+		double goal = Double.parseDouble(request.getParameter("input_goal"));
+		if(goal >= 0.0 && goal <= 4.5) { 
+			score.setGoal(goal);
+			out.println("<script>location.href='average.jsp';</script>");
+		}
+		else {
+			out.println("<script>alert('잘못된 목표입니다.');</script>");
+		}
+	%>
+</body>
+</html>
--- P2P/src/myinfo/ScoreBean.java
+++ P2P/src/myinfo/ScoreBean.java
@@ -8,11 +8,15 @@ public class ScoreBean {
 	private String complete_type;		// 이수구분
 	private int score;					// 학점
 	private int design_score;			// 설계학점
-	private String grade;				// 등급
-	private float average;				// 평점평균
-	ArrayList<Score> score_array = new ArrayList<>();
-	ArrayList<Float> average_array = new ArrayList<>();
+	private double grade;				// 등급
+	private double average;				// 평점평균
+	ArrayList<Score> score_array = new ArrayList<>();		// 평점 평균 빼고 다 집어넣은 리스트
+	ArrayList<Double> average_array = new ArrayList<>();	// 학기별 평점 평균 리스트
+	ArrayList<Major> maverage_array = new ArrayList<>();	// 학기별 전공 평점 평균 리스트
 	
+	private double goal;				// 목표 점수
+	private double total_ave;			// 총 평점 평균
+	private double major_ave;			// 전공 평점 평균
 	
 	public String getSemester() {
 		return semester;
@@ -25,7 +29,7 @@ public class ScoreBean {
 				continue;
 			}
 			else if(buf.get(i).indexOf("평점평균") != -1) {
-				average = Float.parseFloat(buf.get(i).substring(buf.get(i).indexOf("평점평균") + 17));
+				average = Double.parseDouble(buf.get(i).substring(buf.get(i).indexOf("평점평균") + 17));
 				average_array.add(average);
 				continue;
 			}
@@ -36,35 +40,107 @@ public class ScoreBean {
 				design_score = 0;
 			else
 				design_score = Integer.parseInt(buf.get(i + 5));
-			grade = buf.get(i + 6).substring(6);
+			grade = toDoubleGrade(buf.get(i + 6).substring(6));
 			score_array.add(new Score(semester, subject, complete_type, score, design_score, grade));
 			i += 7;
 		}
+		goal = 0;
+		
+		/*
+		 * 학기별 전공 평점 평균 리스트
+		 */
+		int cnt = 0;
+		double ave = 0;
+		String sem_buf = score_array.get(0).getSem();
+		for(Score s : score_array) {
+			if(!sem_buf.equals(s.getSem())) {
+				ave /= cnt;
+				maverage_array.add(new Major(sem_buf, ave));
+				sem_buf = s.getSem();
+				cnt = 0;
+				ave = 0;
+			}
+			if((s.getCom().equals("컴과") || s.getCom().equals("전필")) && s.getGra() != -1) {
+				cnt++;
+				ave += s.getGra();
+			}
+		}
+		ave /= cnt;
+		maverage_array.add(new Major(sem_buf, ave));
+		
+		/*
+		 * 총 평점 평균 구하기 
+		 */
+		total_ave = 0;
+		for(double d : average_array) {
+			total_ave += d;
+		}
+		total_ave /= average_array.size();
+		total_ave = Double.parseDouble(String.format("%.2f", total_ave));
+		
+		/*
+		 * 전공 평점 평균 구하기 
+		 */
+		major_ave = 0;
+		for(Major m : maverage_array) {
+			major_ave += m.getAverage();
+		}
+		major_ave /= maverage_array.size();
+		major_ave = Double.parseDouble(String.format("%.2f", major_ave));
 	}
 	
-	public ArrayList<Score> get_Score() {
+	public ArrayList<Score> getScore() {
 		return score_array;
 	}
 	
-	public ArrayList<Float> getAverage() {
+	public ArrayList<Double> getAverage() {
 		return average_array;
 	}
-}
-
-class Score {
-	private String sem;		// 학기
-	private String sub;		// 교과목명
-	private String com;		// 이수구분
-	private int sco;		// 학점
-	private int des;		// 설계학점
-	private String gra;		// 등급
 	
-	Score(String sem, String sub, String com, int sco, int des, String gra) {
-		this.sem = sem;
-		this.sub = sub;
-		this.com = com;
-		this.sco = sco;
-		this.des = des;
-		this.gra = gra;
+	public ArrayList<Major> getMAverage() {
+		return maverage_array;
+	}
+	
+	public double toDoubleGrade(String s) {
+		double value = 0;
+		
+		if(s.indexOf("A+") != -1)
+			value = 4.5;
+		else if(s.indexOf("A") != -1)
+			value = 4;
+		else if(s.indexOf("B+") != -1)
+			value = 3.5;
+		else if(s.indexOf("B") != -1)
+			value = 3;
+		else if(s.indexOf("C+") != -1)
+			value = 2.5;
+		else if(s.indexOf("C") != -1)
+			value = 2;
+		else if(s.indexOf("D+") != -1)
+			value = 1.5;
+		else if(s.indexOf("D") != -1)
+			value = 1;
+		else if(s.indexOf("F") != -1)
+			value = 0;
+		else if(s.indexOf("P") != -1)
+			value = -1;
+		
+		return value;
+	}
+	
+	public double getGoal() {
+		return goal;
+	}
+	
+	public void setGoal(double value) {
+		goal = value;
+	}
+
+	public double getTotal_ave() {
+		return total_ave;
+	}
+
+	public double getMajor_ave() {
+		return major_ave;
 	}
 }
