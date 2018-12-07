466224d35222b6b773f4f5686e4bd63fd8e5dd65
7926619 <7926619@daum.net>
2018-12-08 04:34:06 +0900

    보안의 중요성

--- P2P/src/connection/DBMainDAO.java
+++ P2P/src/connection/DBMainDAO.java
@@ -2,6 +2,7 @@ package connection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -36,40 +37,55 @@ public class DBMainDAO implements MainDAO{
 	public ArrayList<Integer> count(String id) {
 		// TODO Auto-generated method stub
 		ArrayList<Integer> list=new ArrayList<>();
-		String sql1 = "SELECT count(*) count FROM language where id='"+id+"'";
-		String sql2 = "SELECT count(*) count FROM license where id='"+id+"'";;
-		String sql3 = "SELECT count(*) count FROM award where id='"+id+"'";
-		String sql4 = "SELECT count(*) count FROM career where id='"+id+"'";
-		String sql5 = "SELECT count(*) count FROM service where id='"+id+"'";
+		String sql1 = "SELECT count(*) count FROM language where id=?";
+		String sql2 = "SELECT count(*) count FROM license where id=?";;
+		String sql3 = "SELECT count(*) count FROM award where id=?";
+		String sql4 = "SELECT count(*) count FROM career where id=?";
+		String sql5 = "SELECT count(*) count FROM service where id=?";
 		
 		try {
 			connect();
-			ResultSet rs=stmt.executeQuery(sql1);
+			PreparedStatement pstmt = conn.prepareStatement(sql1);
+			pstmt.setString(1, id);
+			ResultSet rs = pstmt.executeQuery();
 			rs.next();
 			list.add(rs.getInt("count"));
 			rs.close();
+			pstmt.close();
 			
-			ResultSet rs2=stmt.executeQuery(sql2);
+			pstmt = conn.prepareStatement(sql2);
+			pstmt.setString(1, id);
+			ResultSet rs2 = pstmt.executeQuery();
 			rs2.next();
 			list.add(rs2.getInt("count"));
 			rs2.close();
+			pstmt.close();
 			
-			ResultSet rs3=stmt.executeQuery(sql3);
+			pstmt = conn.prepareStatement(sql3);
+			pstmt.setString(1, id);
+			ResultSet rs3 = pstmt.executeQuery();
 			rs3.next();
 			list.add(rs3.getInt("count"));
 			rs3.close();
+			pstmt.close();
 			
-			ResultSet rs4=stmt.executeQuery(sql4);
+			pstmt = conn.prepareStatement(sql4);
+			pstmt.setString(1, id);
+			ResultSet rs4 = pstmt.executeQuery();
 			rs4.next();
 			list.add(rs4.getInt("count"));
 			rs4.close();
+			pstmt.close();
 			
-			ResultSet rs5=stmt.executeQuery(sql5);
+			pstmt = conn.prepareStatement(sql5);
+			pstmt.setString(1, id);
+			ResultSet rs5 = pstmt.executeQuery();
 			rs5.next();
 			list.add(rs5.getInt("count"));
 			rs5.close();
+			pstmt.close();
+			
 			disconnect();
-	
 			
 		} catch(Exception e) {
 			e.printStackTrace();
